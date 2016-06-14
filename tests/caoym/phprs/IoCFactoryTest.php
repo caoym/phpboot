ac0f6118dd1afc14ade20fc63913c440eaeaf663
caoym <caoyangmin@gmail.com>
2016-06-14 14:33:29 +0800

    fix require issue

--- tests/caoym/phprs/IoCFactoryTest.php
+++ tests/caoym/phprs/IoCFactoryTest.php
@@ -2,7 +2,7 @@
 use caoym\util\IoCFactory;
 use caoym\util\Logger;
 
-require_once dirname(dirname(__FILE__)).'/Autoload.php';
+require_once __DIR__.'/../../../lib/caoym/AutoLoad.php';
 
 Logger::$writer = Logger::$to_echo;
 
