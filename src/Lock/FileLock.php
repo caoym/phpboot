c752e60dc597c6a4a483c43ce46910a1323a94f6
caoym <caoyangmin@gmail.com>
2018-06-21 16:38:22 +0800

    fix LocalAutoLock

--- src/Lock/FileLock.php
+++ src/Lock/FileLock.php
@@ -8,7 +8,7 @@ class FileLock implements LockInterface
 
     public function lock($key, $ttl)
     {
-        $path = sys_get_temp_dir().'/lock_252a8fdc9b944af99a9bc53d2aea08f1/'.$key;
+        $path = sys_get_temp_dir().'/lock_252a8fdc9b944af99a9bc53d2aea08f1_'.$key;
         if(SafeFileWriter::write($path, json_encode(['time'=>time(), 'ttl'=>$ttl]), false)){
             $this->locked = true;
             return true;
@@ -20,7 +20,7 @@ class FileLock implements LockInterface
     public function unlock($key)
     {
         $this->locked or \PhpBoot\abort("unlock unlocked $key");
-        $path = sys_get_temp_dir().'/lock_252a8fdc9b944af99a9bc53d2aea08f1/'.$key;
+        $path = sys_get_temp_dir().'/lock_252a8fdc9b944af99a9bc53d2aea08f1_'.$key;
         $res = @unlink($path);
         $this->locked = false;
         return $res;
--- src/Utils/SafeFileWriter.php
+++ src/Utils/SafeFileWriter.php
@@ -19,12 +19,12 @@ class SafeFileWriter
     static public function write($path, $data, $overwrite = true){
         $path = str_replace('\\', '/', $path);
         $fileDir = dirname($path);
-        $tmpFile = @tempnam($fileDir);
+        $tmpFile = tempnam($fileDir, 'safe_writer_');
         false !== @file_put_contents($tmpFile, $data) or \PhpBoot\abort("write to file: $tmpFile failed");
         if($overwrite){
             @unlink($path); //删除原始文件
         }
-        if(!@rename($tmpFile, $path)){
+        if(!rename($tmpFile, $path)){
             @unlink($tmpFile); //删除原始文件
             \PhpBoot\abort("write to file: $tmpFile failed");
             return false;
