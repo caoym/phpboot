86709abc57d69cea93d01bfada8dead607ebee08
caoym <caoyangmin@gmail.com>
2018-06-21 17:18:42 +0800

    fix LocalAutoLock

--- src/Lock/FileLock.php
+++ src/Lock/FileLock.php
@@ -5,25 +5,33 @@ use PhpBoot\Utils\SafeFileWriter;
 
 class FileLock implements LockInterface
 {
-
+    private $file;
     public function lock($key, $ttl)
     {
-        $path = sys_get_temp_dir().'/lock_252a8fdc9b944af99a9bc53d2aea08f1_'.$key;
-        if(SafeFileWriter::write($path, json_encode(['time'=>time(), 'ttl'=>$ttl]), false)){
-            $this->locked = true;
-            return true;
-        }else{
-            return false;
+        if($this->locked){
+            \PhpBoot\abort("relock $key");
         }
+        $path = sys_get_temp_dir().'/lock_252a8fdc9b944af99a9bc53d2aea08f1_'.$key;
+        $this->file = @fopen($path, 'a');
+        if (!$this->file || !flock($this->file, LOCK_EX | LOCK_NB)) {
+            if($this->file){
+                fclose($this->file);
+            }
+            return false;
+        } else {
+            $this->locked = true;
+        }
+        return true;
     }
 
     public function unlock($key)
     {
         $this->locked or \PhpBoot\abort("unlock unlocked $key");
-        $path = sys_get_temp_dir().'/lock_252a8fdc9b944af99a9bc53d2aea08f1_'.$key;
-        $res = @unlink($path);
+        flock($this->file, LOCK_UN);
+        fclose($this->file);
+        $this->file = null;
         $this->locked = false;
-        return $res;
+        return true;
     }
     private $locked = false;
 }
