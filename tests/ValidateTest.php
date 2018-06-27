f1df5c57e692aea869ba8aeb6364666b6371ee5a
caoym <caoyangmin@gmail.com>
2018-06-27 13:30:27 +0800

    fixed validator rule `in`

--- tests/ValidateTest.php
+++ tests/ValidateTest.php
@@ -9,7 +9,43 @@
 namespace PhpBoot\Tests;
 
 
-class ValidateTest
+use PhpBoot\Validator\Validator;
+
+class ValidateTest extends TestCase
 {
+    public function testRuleIn()
+    {
+        $v = new Validator();
+        $v->rule('in:1,2,3', 'a');
+        $res = $v->withData(['a'=>'0'])->validate();
+        self::assertFalse($res);
+
+        $res = $v->withData(['a'=>1])->validate();
+        self::assertTrue($res);
+
+    }
+
+    public function testRuleMinMax()
+    {
+        $v = new Validator();
+        $v->rule('min:1|max:3', 'a');
+        $res = $v->withData(['a'=>'1'])->validate();
+        self::assertTrue($res);
+
+        $res = $v->withData(['a'=>1])->validate();
+        self::assertTrue($res);
+        $res = $v->withData(['a'=>3])->validate();
+        self::assertTrue($res);
+        $res = $v->withData(['a'=>'3'])->validate();
+        self::assertTrue($res);
+
+        $res = $v->withData(['a'=>'0'])->validate();
+        self::assertFalse($res);
+
+        $res = $v->withData(['a'=>'4'])->validate();
+        self::assertFalse($res);
+
+
+    }
 
 }
